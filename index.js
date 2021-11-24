f075df14eedf410cd3d3e18750e38bb77992cebb
TheOrangeDot <theorangedottwitch@gmail.com>
2021-11-25 02:10:32 +0900

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const text = document.querySelector("#text");
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded");
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  text.textContent = "This is really cool!";
+});
